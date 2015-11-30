b57c5cef5afbbff58e8fbb61435c6ed901fbc9c7
sri satti <srisatti.msmit@gmail.com>
2015-11-29 18:55:57 -0600

    recreated the file

---
+++ files/centos-lamp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# update Centos with any patches
+yum update -y --exclude=kernel
+
+#Tools
+yum install -y nano git unzip screen
+
+
+#Apache
+Yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service http start
+
+#PHP
+
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MY Sql
+
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld 
+chkconfig mysqld on
+ 
+
+ service mysqld start 
+
+ mysql -u root -e "Show databases";
+
+ #download starter content
+
+ service apache restart
+
+ 
